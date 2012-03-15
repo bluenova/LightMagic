5ec0be7f3231cd365cdc679b82051ff323f4825d
SvenBrnn <svenbrnn@web.de>
2012-03-15 19:33:28 +0100

    Fix My Fail

--- src/main/java/lightmagic/magics/GlowStoneMagic.java
+++ src/main/java/lightmagic/magics/GlowStoneMagic.java
@@ -6,8 +6,8 @@ package lightmagic.magics;
 
 import bluenova.fairytailcraft.plugin.MagePluginEvent;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.inventory.CraftItemStack;
-import org.bukkit.event.Event.Result;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -23,11 +23,10 @@ public class GlowStoneMagic extends MagePluginEvent {
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             ItemStack item = event.getItem();
             if(item == null) {
-                item = new CraftItemStack(Material.GLOWSTONE, 1);
                 event.getPlayer().setItemInHand(item);
-                Result useItemInHand = event.useItemInHand();
-                //event.setUseItemInHand(useItemInHand);
-                event.getPlayer().setItemInHand(null);
+                Block clickedBlock = event.getClickedBlock();
+                BlockFace blockFace = event.getBlockFace();
+                clickedBlock.getRelative(blockFace).setType(Material.TORCH);
                 return true;
             }
         }
--- src/main/java/lightmagic/magics/SummonGlowstones.java
+++ src/main/java/lightmagic/magics/SummonGlowstones.java
@@ -6,10 +6,7 @@ package lightmagic.magics;
 
 import bluenova.fairytailcraft.plugin.MagePluginEvent;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
-import org.bukkit.event.Event.Result;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -25,10 +22,8 @@ public class SummonGlowstones extends MagePluginEvent {
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR ) {
             ItemStack item = event.getItem();
             if(item == null) {
+                item = new CraftItemStack(Material.GLOWSTONE, 64);
                 event.getPlayer().setItemInHand(item);
-                Block clickedBlock = event.getClickedBlock();
-                BlockFace blockFace = event.getBlockFace();
-                clickedBlock.getRelative(blockFace).setType(Material.TORCH);
                 return true;
             }
         }

c6ea3e86d74b45c5864c429dae597f8d454226f0
SvenBrnn <svenbrnn@web.de>
2012-03-19 12:36:44 +0100

    Fix it

--- src/main/java/lightmagic/magics/FairyLaw.java
+++ src/main/java/lightmagic/magics/FairyLaw.java
@@ -14,7 +14,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World.Environment;
-import org.bukkit.WorldType;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -31,8 +30,22 @@ public class FairyLaw extends MagePluginEvent {
     @Override
     public boolean callPlayerInteractEvent(PlayerInteractEvent event) {
         if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getPlayer().getLocation().getWorld().getEnvironment() == Environment.NORMAL) {
+            new fairyLawThread(event).start();
+            return true;
+        }
+        return false;
+    }
+
+    class fairyLawThread extends Thread {
+
+        public PlayerInteractEvent event;
+        public fairyLawThread(PlayerInteractEvent event) {
+            this.event = event;
+        }
+        
+        public void run() {
             Player play = event.getPlayer();
-            List<Entity> nearbyEntities = event.getPlayer().getNearbyEntities(100, 256, 100);
+            List<Entity> nearbyEntities = event.getPlayer().getNearbyEntities(30, 256, 30);
             List<Player> warnList = new ArrayList<Player>();
             for (Entity ent : nearbyEntities) {
                 if (ent instanceof LivingEntity) {
@@ -63,26 +76,39 @@ public class FairyLaw extends MagePluginEvent {
                 }
             }
             if (!event.getPlayer().isDead()) {
-                nearbyEntities = event.getPlayer().getNearbyEntities(100, 256, 100);
+                nearbyEntities = event.getPlayer().getNearbyEntities(30, 256, 30);
                 List<LivingEntity> toKill = new ArrayList<LivingEntity>();
                 for (Entity ent : nearbyEntities) {
                     if (ent instanceof LivingEntity) {
                         toKill.add((LivingEntity) ent);
                     }
                 }
-                int hight = event.getPlayer().getLocation().getWorld().getMaxHeight();
                 Location loc = event.getPlayer().getLocation();
-                Location loc1 = new Location(loc.getWorld(), loc.getBlockX() - 100, hight, loc.getBlockZ() - 100);
-                Location loc2 = new Location(loc.getWorld(), loc.getBlockX() + 100, hight, loc.getBlockZ() + 100);
-                MagePluginRegion regio = new MagePluginRegion(loc1, loc2, loc.getWorld());
-                List<Block> blocks = regio.getBlocks();
-                for (Block blk : blocks) {
-                    if (blk.getType() != Material.AIR) {
-                        blocks.remove(blk);
+                int hight;
+                if (loc.getBlockY() + 20 > event.getPlayer().getLocation().getWorld().getMaxHeight()) {
+                    hight = event.getPlayer().getLocation().getWorld().getMaxHeight();
+                } else {
+                    hight = loc.getBlockY() + 20;
+                }
+                
+                
+                List<Block> blocks = new ArrayList<Block>();
+                Location entloc;
+                MagePluginRegion entR;
+                for (LivingEntity ent : toKill) {
+                    entloc = ent.getLocation();
+                    entR = new MagePluginRegion(new Location(entloc.getWorld(), entloc.getBlockX()-1, hight, entloc.getBlockZ()-1), new Location(entloc.getWorld(), entloc.getBlockX()+1, hight, entloc.getBlockZ()+1), entloc.getWorld());
+                    List<Block> blocks1 = entR.getBlocks();
+                    for(Block b : blocks1){
+                        if(b.getType() == Material.AIR)
+                            blocks.add(b);
                     }
                 }
+                
                 for (Block blk : blocks) {
                     blk.setType(Material.GLOWSTONE);
+                    //System.out.println("Glowstone!");
+
                 }
                 try {
                     Thread.sleep(500);
@@ -102,7 +128,7 @@ public class FairyLaw extends MagePluginEvent {
                     ent.damage(ent.getHealth() + 10000, event.getPlayer());
                 }
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(30000);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(FairyLaw.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -111,6 +137,5 @@ public class FairyLaw extends MagePluginEvent {
                 }
             }
         }
-        return false;
     }
 }
